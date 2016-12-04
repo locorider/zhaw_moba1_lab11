4b7c7041163ea19bc6178aa6ecc32e58aa0f74b3
José Miguel Rota <jose@kinastic.com>
2016-12-04 13:02:37 +0100

    Initial Commit

---
+++ zhaw_moba1_lab11/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  zhaw_moba1_lab11
+//
+//  Created by José Miguel Rota on 04.12.16.
+//  Copyright © 2016 José Miguel Rota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
